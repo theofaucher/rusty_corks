700c8f8b2c59f00c3e695e6e7c99a9756e5576cb
Théo FAUCHER <theo.freelance@fchr.fr>
2023-10-11 10:20:36 +0200

    Ajout du stop du jeu et intégration de LockError
    Signed-off-by: Théo FAUCHER <theo.freelance@fchr.fr>

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,8 +1,6 @@
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::AtomicBool;
 use std::sync::mpsc::Receiver;
-use std::thread;
 
 use macroquad::input::KeyCode;
 use macroquad::prelude::get_frame_time;
@@ -11,17 +9,24 @@ use crate::game::car::bot_car::BotCar;
 use crate::game::car::player_car::PlayerCar;
 use crate::game::car::Way;
 use crate::game::graphics::graphics_manager::GraphicsManager;
-use crate::utils::rusty_error::{RustyError, RustyResult};
+use crate::utils::rusty_error::{LockError, RustyError, RustyResult};
+use crate::utils::rusty_error::RustyError::RustyLock;
 use crate::utils::timer::{Timer, TimerData};
 
-#[derive(Clone)]
+const PLAYER_INPUT_AND_CAR_REACTION: [(usize, Way, Way); 4] = [
+    (KeyCode::Up as usize, Way::Center, Way::Upper),
+    (KeyCode::Up as usize, Way::Lower, Way::Center),
+    (KeyCode::Down as usize, Way::Upper, Way::Center),
+    (KeyCode::Down as usize, Way::Center, Way::Lower),
+];
+
+#[derive(Clone, PartialEq)]
 enum GameState {
     NotStarted,
     Running,
     GameOver,
 }
 
-#[derive(Clone)]
 pub struct Game {
     receiver_input: Arc<Mutex<Receiver<KeyCode>>>,
     running: Arc<AtomicBool<>>,
@@ -30,7 +35,9 @@ pub struct Game {
     bot_cars: Vec<BotCar>,
     game_timer: Timer,
     pub score: Arc<Mutex<u32>>,
+    session_record: u32,
     game_state: GameState,
+    game_previous_state: GameState,
 }
 
 impl Game {
@@ -49,31 +56,128 @@ impl Game {
             bot_cars: Vec::new(),
             game_timer: Timer::new(Game::add_score, Arc::new(Mutex::new(TimerData::GameScore { score: Arc::clone(&score) }))),
             score: Arc::clone(&score),
+            session_record: 0,
             game_state: GameState::NotStarted,
+            game_previous_state: GameState::NotStarted,
         })
     }
 
-    fn add_score(timer_data: &mut TimerData) {
-        let TimerData::GameScore { score: game_score } = timer_data;
-        let game_score_lock = game_score.lock();
-
-        match game_score_lock {
-            Ok(mut init_score) => *init_score += 1,
-            Err(e) => {
-                println!("Error lock current score: {}", e);
-            }
+    pub fn start(&mut self) -> RustyResult<()> {
+        let current_score = self.score.lock();
+        let mut current_score = match current_score {
+            Ok(current_score) => current_score,
+            Err(_) => Err(RustyLock(LockError {
+                message: "Impossible to lock the access to the current score".to_string(),
+            }))?,
         };
+
+        *current_score = 0;
+
+        self.game_timer.start(1000);
+        self.game_state = GameState::Running;
+
+        Ok(())
     }
 
-    pub fn start(&mut self) {
-        let game_clone = self.clone();
-        self.game_timer.start(500);
+    pub async fn run(&mut self) -> RustyResult<()> {
+        let player_input = self.get_keyboard_input()?;
 
-        thread::spawn(move || {
-            game_clone.move_player_car();
-        });
+        let delta_time = get_frame_time();
 
-        self.game_state = GameState::Running;
+        self.graphics_manager.background.move_texture(delta_time);
+
+        let entrance: bool = self.game_state != self.game_previous_state;
+
+        match self.game_state {
+            GameState::NotStarted => {
+                if player_input == KeyCode::Enter {
+                    self.start()?;
+                } else {
+                    self.graphics_manager.draw_new_game();
+                }
+            }
+            GameState::Running => {
+                self.move_player_car(player_input)?;
+
+                let player_car = self.player_car.lock();
+                let player_car = match player_car {
+                    Ok(player_car) => player_car,
+                    Err(_) => Err(RustyLock(LockError {
+                        message: "Impossible to lock the access to the player input".to_string(),
+                    }))?,
+                };
+
+                self.graphics_manager.draw_player_car(player_car);
+
+                let current_score = self.score.lock();
+                let current_score = match current_score {
+                    Ok(current_score) => current_score,
+                    Err(_) => Err(RustyLock(LockError {
+                        message: "Impossible to lock the access to the current score".to_string(),
+                    }))?,
+                };
+
+                self.graphics_manager.draw_score(*current_score);
+                if player_input == KeyCode::Enter {
+                    self.game_state = GameState::GameOver;
+                }
+            }
+            GameState::GameOver => {
+                if entrance {
+                    self.stop()?;
+                }
+
+                let current_score = self.score.lock();
+                let current_score = match current_score {
+                    Ok(current_score) => current_score,
+                    Err(_) => Err(RustyLock(LockError {
+                        message: "Impossible to lock the access to the current score".to_string(),
+                    }))?,
+                };
+
+                self.graphics_manager.draw_game_over(*current_score, self.session_record);
+
+                if player_input == KeyCode::Enter {
+                    self.game_state = GameState::NotStarted;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn stop(&mut self) -> RustyResult<()> {
+        self.game_timer.stop();
+
+        let current_score = self.score.lock();
+        let current_score = match current_score {
+            Ok(current_score) => current_score,
+            Err(_) => Err(RustyLock(LockError {
+                message: "Impossible to lock the access to the current score".to_string(),
+            }))?,
+        };
+
+        if self.session_record < *current_score {
+            self.session_record = *current_score;
+        }
+
+        Ok(())
+    }
+
+    fn move_player_car(&self, player_input: KeyCode) -> RustyResult<()> {
+        let player_car = self.player_car.lock();
+        let mut player_car = match player_car {
+            Ok(player_car) => player_car,
+            Err(_) => Err(RustyLock(LockError {
+                message: "Impossible to lock the access to the player car".to_string(),
+            }))?,
+        };
+
+        if let Some(new_way) = Game::get_destination_way(player_input as usize, player_car.way) {
+            player_car.way = new_way;
+        }
+
+        Ok(())
     }
 
     fn get_keyboard_input(&self) -> RustyResult<KeyCode> {
@@ -91,82 +195,32 @@ impl Game {
                 }
             }
         } else {
-            Err(RustyError::RustyLock)
+            Err(RustyLock(LockError {
+                message: "Impossible to lock the access to the player car".to_string(),
+            }))?
         }
     }
 
-    pub async fn run(&mut self) -> RustyResult<()> {
-        let player_input = self.get_keyboard_input()?;
+    fn add_score(timer_data: &mut TimerData) {
+        let TimerData::GameScore { score: game_score } = timer_data;
+        let game_score_lock = game_score.lock();
 
-        let delta_time = get_frame_time();
-
-        self.graphics_manager.background.update_position(delta_time);
-
-        match self.game_state {
-            GameState::NotStarted => {
-                if player_input == KeyCode::Enter {
-                    self.start();
-                } else {
-                    self.graphics_manager.draw_new_game();
-                }
+        match game_score_lock {
+            Ok(mut init_score) => *init_score += 1,
+            Err(e) => {
+                println!("Error lock current score: {}", e);
             }
-            GameState::Running => {
-                let player_car = self.player_car.lock();
-                let player_car = match player_car {
-                    Ok(player_car) => player_car,
-                    Err(_) => Err(RustyError::RustyLock)?,
-                };
-
-                self.graphics_manager.draw_player_car(player_car);
-
-                let current_score = self.score.lock();
-                let current_score = match current_score {
-                    Ok(current_score) => current_score,
-                    Err(_) => Err(RustyError::RustyLock)?,
-                };
-
-                self.graphics_manager.draw_score(*current_score);
-            }
-            GameState::GameOver => {
-                println!("Game over");
-            }
-        }
-
-        Ok(())
+        };
     }
 
-    pub fn move_player_car(&self) {
-        let mut player_input_and_car_reaction: HashMap<(usize, Way), Way> = HashMap::new();
-
-        player_input_and_car_reaction.insert((KeyCode::Up as usize, Way::Center), Way::Upper);
-        player_input_and_car_reaction.insert((KeyCode::Up as usize, Way::Lower), Way::Center);
-        player_input_and_car_reaction.insert((KeyCode::Down as usize, Way::Upper), Way::Center);
-        player_input_and_car_reaction.insert((KeyCode::Down as usize, Way::Center), Way::Lower);
-
-
-        while self.running.load(Ordering::Relaxed) {
-            let player_input = self.get_keyboard_input();
-            let player_input = match player_input {
-                Ok(player_input) => player_input as usize,
-                Err(e) => {
-                    println!("Error getting player input: {}", e);
-                    break;
-                }
-            };
-
-            let player_car_lock = self.player_car.lock();
-
-            let mut player_car = match player_car_lock {
-                Ok(player_car) => player_car,
-                Err(e) => {
-                    println!("Error locking player car: {}", e);
-                    break;
-                }
-            };
-
-            if let Some(new_way) = player_input_and_car_reaction.get(&(player_input, player_car.way)) {
-                player_car.way = *new_way;
+    fn get_destination_way(key_code: usize, way: Way) -> Option<Way> {
+        let mut destination_way: Option<Way> = None;
+        for &(k, w1, w2) in &PLAYER_INPUT_AND_CAR_REACTION {
+            if k == key_code && w1 == way {
+                destination_way = Some(w2);
+                break;
             }
         }
+        destination_way
     }
 }
